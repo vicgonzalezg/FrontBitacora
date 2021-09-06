a58ee928dfcb9a7d81a447c15d24c4deecd1dc4d
Víctor Hernán González Gallardo <vic.gonzalezg@duocuc.cl>
2021-09-06 20:23:34 -0300

    se agrega menu, falta seguir

--- bitacoraProject/frontProject/urls.py
+++ bitacoraProject/frontProject/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.login),
+    path('menu/', views.menu),
 ]
--- bitacoraProject/frontProject/views.py
+++ bitacoraProject/frontProject/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+#definimos el login
 def login(request):
     return render(request, 'login/login.html')
+#definimos el menu
+def menu(request):
+    return render(request, 'menu/menu.html')

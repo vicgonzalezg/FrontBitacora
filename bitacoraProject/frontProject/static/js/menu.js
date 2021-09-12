194cf275f2f9cdc0ff102d1ba851e257dcad1506
Carlos Jatib <carlos.jatib@chile.taylor.cl>
2021-09-12 02:12:09 -0300

    Se corrige navbar y menu responsivo.

--- bitacoraProject/frontProject/static/js/menu.js
+++ bitacoraProject/frontProject/static/js/menu.js
@@ -1,7 +1,15 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+    var mq = window.matchMedia( "(mim-width: 450px)" );
+  if (mq.matches) {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  else {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "50px";
+  }
+  
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */

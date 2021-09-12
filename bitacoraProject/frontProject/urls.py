e2fbc09365d6fa680d23a2d01a5e354808e2056f
Carlos Jatib <carlos.jatib@chile.taylor.cl>
2021-09-12 19:05:06 -0300

    Se agrega menu Coach y Coachee y otras modificaciones a css

--- bitacoraProject/frontProject/urls.py
+++ bitacoraProject/frontProject/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.login),
+    path('/', views.login),
     path('menu/', views.menu),
 ]
--- bitacoraProject/frontProject/views.py
+++ bitacoraProject/frontProject/views.py
@@ -1,10 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
 # Create your views here.
 
 #definimos el login
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('user')
+        password = request.POST.get('pass')
+        print(username,password)
+        if username == 'maria.jose' and password == 'inicio2021':
+            return render(request,'menu/menuAdmin.html')
+        elif username== 'nelson.gomez' and password == 'inicio2021':
+            return render(request,'menu/menuCoach.html')
+        elif username== 'victor.gonzalez' and password == 'inicio2021':
+            return render(request,'menu/menuCoachee.html')
     return render(request, 'login/login.html')
+
 #definimos el menu
 def menu(request):
-    return render(request, 'menu/menu.html')
+    if request.method == 'POST':
+        username = request.POST.get('user')
+        password = request.POST.get('pass')
+        print(username,password)
+        if username == 'maria.jose' and password == 'inicio2021':
+            return render(request,'menu/menuAdmin.html')
+        elif username== 'juan.topo' and password == 'inicio2021':
+            return render(request,'menu/menuCoach.html')
+
+    return render(request, 'login/login.html')
+
+
+def menuAdmin(request):
+    return render(request,'menu/menuAdmin.html')
+
+def menuCoach(request):
+    return render(request,'menu/menuCoach.html')
+
+
+def menuCoachee(request):
+    return render(request,'menu/menuCoachee.html')
+
